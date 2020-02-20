3d4a3828f282f16c73badb03f78c2970bb4f8b72
Brandon Peach <brandon.peach.2022@student.lhschools.org>
2020-02-20 11:02:42 -0500

    intellij commit

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,126 @@
+package com.LickingHeights;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+	//Phase 1
+
+    String name;
+    String greeting;
+    String song;
+    String adjective;
+    String famousPerson;
+    String actionWordEndingInIng;
+    String object;
+    String feeling;
+    String anotherObject;
+    String animal;
+    String familyMember;
+    String verbEndingInIng;
+    String food;
+    String celebrity;
+    String location;
+    String item;
+    String candy;
+    String verbExpressingAnger;
+    String videoGame;
+    String socialMedia;
+
+
+    //Phase 2
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Enter a name");
+        name = keyboard.nextLine();
+        System.out.println("Enter a greeting");
+        greeting = keyboard.nextLine();
+        System.out.println("Enter a song");
+        song = keyboard.nextLine();
+        System.out.println("Enter an adjective");
+        adjective = keyboard.nextLine();
+        System.out.println("Enter a historical person");
+        famousPerson = keyboard.nextLine();
+        System.out.print("Enter an action word ending in -ing");
+        actionWordEndingInIng = keyboard.nextLine();
+        System.out.println("Enter an object");;
+        object = keyboard.nextLine();
+        System.out.println("Enter a feeling");
+        feeling = keyboard.nextLine();
+        System.out.println("Enter a different object from before");
+        anotherObject = keyboard.nextLine();
+        System.out.println("Enter an animal");
+        animal = keyboard.nextLine();
+        System.out.println("Enter a family member (ex; brother, mother...");
+        familyMember = keyboard.nextLine();
+        System.out.println("Enter a verb ending in ing");
+        verbEndingInIng = keyboard.nextLine();
+        System.out.println("Enter the name of a celebrity");
+        celebrity = keyboard.nextLine();
+        System.out.println("Enter a house item");
+        item = keyboard.nextLine();
+        System.out.println("Enter a type of candy");
+        candy = keyboard.nextLine();
+        System.out.println("Enter a verb expressing anger (ex: throw");
+        verbExpressingAnger = keyboard.nextLine();
+        System.out.println("Enter a video game");
+        videoGame = keyboard.nextLine();
+        System.out.println("Enter a social media app");
+        socialMedia = keyboard.nextLine();
+
+
+
+        //Phase 3
+        System.out.println("One day, "+name+" went out for a walk and said "+greeting+" to everyone in the town");
+        System.out.println(name+" sang "+song+" as he walked; it seemed to be a "+adjective+" day.");
+        System.out.println("Strangely, "+name+" saw "+famousPerson+" walking on the street.");
+        System.out.println(name+" decided to start "+actionWordEndingInIng+" towards "+famousPerson);
+        System.out.println("Surprised, "+famousPerson+" threw a "+object+" at "+name);
+        System.out.println("This caused "+name+" to feel "+feeling);
+        System.out.println("Suddenly, "+famousPerson+" pulled out a "+anotherObject);
+        System.out.println("Just as "+famousPerson+" was about to throw the object, a "+animal+" knocked out "+name);
+        System.out.println("When "+name+" came conscious, their "+familyMember+" appeared as well as "+celebrity);
+        System.out.println("The celebrity handed "+name+" a "+item+" to "+verbExpressingAnger+ " at "+famousPerson);
+        System.out.print(name+" successfully hit "+famousPerson+" and got "+videoGame+" for free ");
+        System.out.println("As well as a follow on "+socialMedia);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
